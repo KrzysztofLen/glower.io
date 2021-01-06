0eb64041f49304f1cb7a7d5c9f8e501a6badf78c
Krzysztof Len <len.krzysztof@gmail.com>
2021-01-06 17:05:59 +0100

    chore: add React & config to jekyll (#11)
    * chore: add React & config to jekyll
    
    * chore: add stylint & commitlint configs
    
    * chore: update package.json

---
+++ docs/README.md
@@ -0,0 +1,7 @@
+Run all commands simultaneously in separate terminals.
+
+```npm run webpack```
+
+```npm run build```
+
+```npm run start```
---
+++ docs/webpack/components/Hello.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+class Hello extends Component {
+	render() {
+		return <div>Hey, test</div>;
+	}
+}
+export default Hello;
