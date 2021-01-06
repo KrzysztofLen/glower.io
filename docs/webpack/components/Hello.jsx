b891def1fb6ea66778def6457376e2f9502270a8
KrzysztofLen <len.krzysztof@gmail.com>
2021-01-06 17:52:07 +0100

    chore: change docs structure due to github pages

--- docs/webpack/components/Hello.jsx
+++ docs/webpack/components/Hello.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 class Hello extends Component {
-	render() {
-		return <div>Hey, test</div>;
-	}
+  render() {
+    return <div>Hey, new one</div>;
+  }
 }
 export default Hello;
